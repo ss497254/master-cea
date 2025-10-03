8ba0a2f96e70ad90dcd0d23ed0c2c26c2ac0ed2a
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2025-10-03 10:22:36 +0000

    Add sendCard function to facilitate sending adaptive cards

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,24 @@
 import { Activity, ActivityTypes } from '@microsoft/agents-activity';
+import { TurnContext } from '@microsoft/agents-hosting';
 
 export function getMessageTextFromActivity(activity: Activity): string | undefined {
   if (Activity.fromObject(activity).type === ActivityTypes.Message) {
     return activity.text;
   }
+}
+
+export function sendCard(context: TurnContext, card: object) {
+  return sendActivity(context, {
+    type: ActivityTypes.Message,
+    attachments: [
+      {
+        contentType: 'application/vnd.microsoft.card.adaptive',
+        content: card,
+      },
+    ],
+  });
+}
+
+export function sendActivity(context: TurnContext, activity: object) {
+  return context.sendActivity(Activity.fromObject(activity));
 }
